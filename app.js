da3667dcb694915667d93afaf674c5fc3c82e23b
leaderg <leader.g@gmail.com>
2019-05-17 20:45:53 +0000

    Initial Set up of Server and Image upload capabilities.

---
+++ app.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const PORT = 8080;
+const express = require('express');
+const app = express();
+const vibrant = require('node-vibrant');
+const fs = require('fs');
+const multer  = require('multer');
+
+var storage = multer.diskStorage(
+    {
+        destination: './uploads',
+        filename: function ( req, file, cb ) {
+            //req.body is empty...
+            //How could I get the new_file_name property sent from client here?
+            cb( null, "img");
+        }
+    }
+);
+
+const upload = multer({ storage: storage });
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server Initialized on port: ${PORT}.`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
+})
+
+app.post("/upload", upload.single('image'), function (req, res, next) {
+  console.log(req.file);
+  res.send('<a href="/">Go back</a>');
+})
